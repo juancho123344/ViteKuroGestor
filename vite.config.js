547a4929988ee4bc8b1228c621b0592e3f6fe879
juancho123344 <juanjosesilver1@gmail.com>
2024-06-13 15:32:53 -0500

    vite vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import swc from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react(), swc()],
+  plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist', // Directorio de salida de la construcción
+    emptyOutDir: true, // Limpiar directorio de salida antes de construir
+    sourcemap: true, // Habilitar sourcemaps para depuración
+  },
 });
