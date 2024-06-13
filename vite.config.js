1ba251208ad6ea6fc0613484b7e27f6c23659f57
juancho123344 <juanjosesilver1@gmail.com>
2024-06-13 15:28:30 -0500

    Hola

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import swc from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), swc()],
   build: {
-    outDir: 'build', // Aquí especifica el directorio de salida deseado
-  },
-})
+    outDir: 'dist'
+  }
+});
